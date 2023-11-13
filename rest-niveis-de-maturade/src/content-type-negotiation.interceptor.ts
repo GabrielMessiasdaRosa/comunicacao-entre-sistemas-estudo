43413e972ef4f5920cd03f5e8c677d3e86734559
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-11-13 10:50:33 -0300

    feat: create method && content-type negotiation

--- rest-niveis-de-maturade/src/content-type-negotiation.interceptor.ts
+++ rest-niveis-de-maturade/src/content-type-negotiation.interceptor.ts
@@ -1,10 +1,12 @@
 import { Injectable, UnsupportedMediaTypeException } from '@nestjs/common';
 
+const CONTENT_TYPE_WHITE_LIST = ['application/json', 'application/hal_json'];
+
 @Injectable()
 export class ContentTypeNegotiationInterceptor {
   intercept(context, next) {
     const request = context.switchToHttp().getRequest();
-    if (request.headers['content-type'] !== 'application/json') {
+    if (!CONTENT_TYPE_WHITE_LIST.includes(request.headers['content-type'])) {
       throw new UnsupportedMediaTypeException();
     }
     return next.handle();
--- rest-niveis-de-maturade/src/method-negotiation.interceptor.ts
+++ rest-niveis-de-maturade/src/method-negotiation.interceptor.ts
@@ -1,10 +1,12 @@
 import { Injectable, MethodNotAllowedException } from '@nestjs/common';
 
+const METHOD_WHITE_LIST = ['GET', 'POST', 'PUT'];
+
 @Injectable()
 export class MethodNegotiationInterceptor {
   intercept(context, next) {
     const request = context.switchToHttp().getRequest();
-    if (request.method !== 'GET') {
+    if (!METHOD_WHITE_LIST.includes(request.method)) {
       throw new MethodNotAllowedException();
     }
     return next.handle();
